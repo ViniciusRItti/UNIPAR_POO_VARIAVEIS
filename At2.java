94aff78b57d53f060b86e2b3a21df6badc42d866
ViniciusRitti <106410723+ViniciusRItti@users.noreply.github.com>
2024-02-20 22:23:45 -0300

    Create At2.java

---
+++ At2.java
@@ -0,0 +1,10 @@
+1. 1nomeProduto
+2. preçoProduto%
+3. marca do produto
+4. quantidade produto
+5. _descricaoProduto
+6. NomeProduto
+7. 123_numeroProduto
+8. precoDoProduto
+9. estoque Disponivel
+10. totaldeProdutos
