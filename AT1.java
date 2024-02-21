c2cdd8488f77b4dfb95d86e4a1d12d864d8edb76
ViniciusRitti <106410723+ViniciusRItti@users.noreply.github.com>
2024-02-20 22:17:18 -0300

    Create AT1.java

---
+++ AT1.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String nome;
+        float valor = 1.2f;
+        int idade = 10;
+        Integer ano = 50;
+        double peso = 80.5;
+        boolean confirma = true;
+        char sexo = 'M';
+        long numG = 323432423434;
+        byte numP = 2334
+        short numC = 123
+    }
+}
