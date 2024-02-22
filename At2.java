f2eebd4048f514ff51798f91ededa8ee09967cf0
ViniciusRitti <106410723+ViniciusRItti@users.noreply.github.com>
2024-02-22 14:11:52 -0300

    Update At2.java

--- At2.java
+++ At2.java
@@ -1,10 +1,13 @@
-1. 1nomeProduto
-2. preçoProduto%
-3. marca do produto
-4. quantidade produto
-5. _descricaoProduto
-6. NomeProduto
-7. 123_numeroProduto
-8. precoDoProduto
-9. estoque Disponivel
-10. totaldeProdutos
+public class At2 {
+    private String nomeProduto;
+    private double precoProduto; 
+    private String marcaDoProduto; 
+    private int quantidadeProduto; 
+    private String descricaoProduto; 
+    private String nomeDoProduto;
+    private int numeroProduto; 
+    private double precoDoProduto; 
+    private int estoqueDisponivel; 
+    private int totalDeProdutos; 
+}
+
