ad5afb6e573ccda829108579af5689cf8a441867
ViniciusRitti <106410723+ViniciusRItti@users.noreply.github.com>
2024-02-22 15:36:45 -0300

    Create AT6.java

---
+++ AT6.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        int numeroProduto = 123; 
+        double precoProduto = 25; 
+        
+        String descricaoProduto;
+        descricaoProduto = "Fone de ouvido Bluetooth";
+        
+        System.out.println("Descricao do Produto: " + descricaoProduto);
+        
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Nuumero: " + numeroProduto);
+        System.out.println("Preco: $" + precoProduto);
+        System.out.println("Descricao: " + descricaoProduto);
+    }
+}
