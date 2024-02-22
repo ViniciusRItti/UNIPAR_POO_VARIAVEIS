d60485bb2a46d1c0290ed00c22fa25392db55a97
ViniciusRitti <106410723+ViniciusRItti@users.noreply.github.com>
2024-02-22 14:18:48 -0300

    Update and rename At2.java to AT2.java

---
+++ AT2.java
@@ -0,0 +1,14 @@
+class AT2 {
+    public static void main(String[] args) {
+        String nomeProduto;
+        double precoProduto; 
+        String marcaDoProduto; 
+        int quantidadeProduto; 
+        String descricaoProduto; 
+        String nomeDoProduto;
+        int numeroProduto; 
+        double precoDoProduto; 
+        int estoqueDisponivel; 
+        int totalDeProdutos;
+    }
+}
