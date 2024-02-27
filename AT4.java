92ff326fdd7c09aff15b76f19fecc0ee18591706
ViniciusRitti <106410723+ViniciusRItti@users.noreply.github.com>
2024-02-27 16:46:52 -0300

    Update AT4.java

--- AT4.java
+++ AT4.java
@@ -1,6 +1,6 @@
 public class ControleEstoque {
     public static void main(String[] args) {
-        // Declaração das variáveis aqui
+        
         String nomeProduto;
         String categoriaProduto
         double precoUnitario;
