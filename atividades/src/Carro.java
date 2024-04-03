12892bb68e738a499a0a9378d12bc8de0610dfca
ViniciusRitti <106410723+ViniciusRItti@users.noreply.github.com>
2024-04-02 22:16:54 -0300

    Add files via upload

---
+++ atividades/src/Carro.java
@@ -0,0 +1,28 @@
+class Carro {
+
+    String marca;
+    String modelo;
+    int ano;
+    String cor;
+    double preco;
+
+
+    void ligar() {
+        System.out.println("O carro está ligado.");
+    }
+
+
+    void desligar() {
+        System.out.println("O carro está desligado.");
+    }
+
+
+    void acelerar() {
+        System.out.println("O carro está acelerando.");
+    }
+
+
+    void frear() {
+        System.out.println("O carro está freando.");
+    }
+}
